ba2417cf0f4ce7feed72b44623d3d3645f4b2560
Clement <clem83f@hotmail.fr>
2015-03-15 00:29:22 +0100

    Now able to find all objects in a box, still missing a sumup info of the content of the subtree in a root node, for looking in neighbourhood or for speed force calculation

--- qTree.js
+++ qTree.js
@@ -13,13 +13,39 @@ QTree.prototype.add = function(content, contentLoc){
 	this.root.put(content, contentLoc);
 };
 
+QTree.prototype.findIn = function(left, top, right, bot){
+	var output = [];
+	var process = [this.root];
+	var next = null;
+	while(next = process.pop()){
+		if(next.status == 'root'){ //Explore the tree to register new elements
+			if(next.bpIn(left, top, right, bot)){
+				next.content.map(function(c){
+					process.push(c);
+				});
+			}
+			else {
+				var index = [next.getQuad([left, top]), next.getQuad([right, bot])];
+				for(iii=index[0]; iii<= index[1]; iii++){
+					process.push(next.content[iii]);
+				}
+			}
+		}
+		else {
+			if(next.content && next.contentIn(left, top, right, bot)) output.push(next.content);
+		}
+	}
+	
+	return output;
+};
+
+QTree.prototype.deploy = function(){
+	return this.root.deployContent();
+};
+
 function QNode(level, breakPoint){
 	this.breakPoint = breakPoint;
 	this.status = 'leaf';
-	this.NW = null;
-	this.NE = null;
-	this.SW = null;
-	this.SE = null;
 	this.content = null;
 	this.contentLoc = null;
 	this.lvl = level;
@@ -32,10 +58,10 @@ QNode.prototype.put = function(content, contentLoc){
 	}
 	else if(this.status == 'leaf'){
 		this.expand();
-		this[this.getQuad(contentLoc)].put(content, contentLoc);
+		this.content[this.getQuad(contentLoc)].put(content, contentLoc);
 	}
 	else {
-		this[this.getQuad(contentLoc)].put(content, contentLoc);
+		this.content[this.getQuad(contentLoc)].put(content, contentLoc);
 	}
 };
 
@@ -44,18 +70,47 @@ QNode.prototype.expand = function(){
 	var bp = this.breakPoint;
 	
 	this.status = 'root';
-	this.NW = new QNode(level, [bp[0] - 0.25/level, bp[1] - 0.25/level]);
-	this.NE = new QNode(level, [bp[0] + 0.25/level, bp[1] - 0.25/level]);
-	this.SW = new QNode(level, [bp[0] - 0.25/level, bp[1] + 0.25/level]);
-	this.SE = new QNode(level, [bp[0] + 0.25/level, bp[1] + 0.25/level]);
-	this[this.getQuad(this.contentLoc)].put(this.content, this.contentLoc);
-	this.content = null;
+	this.content = [this.content];
+	this.content.push(new QNode(level, [bp[0] - 0.25/level, bp[1] - 0.25/level]));
+	this.content.push(new QNode(level, [bp[0] + 0.25/level, bp[1] - 0.25/level]));
+	this.content.push(new QNode(level, [bp[0] - 0.25/level, bp[1] + 0.25/level]));
+	this.content.push(new QNode(level, [bp[0] + 0.25/level, bp[1] + 0.25/level]));
+	
+	var cont = this.content.shift();
+	this.content[this.getQuad(this.contentLoc)].put(cont, this.contentLoc);
 	this.contentLoc = null;
 };
 
 QNode.prototype.getQuad = function(contentLoc){
 	var bp = this.breakPoint;
-	var loc = (contentLoc[1] <= bp[1]) ? 'N' : 'S';
-	loc += (contentLoc[0] <= bp[0]) ? 'W' : 'E';
+	var loc = (contentLoc[1] <= bp[1]) ? 0 : 2;
+	loc += (contentLoc[0] <= bp[0]) ? 0 : 1;
 	return loc;
 };
+
+QNode.prototype.bpIn = function(left, top, right, bot){
+	var bp = this.breakPoint;
+	return bp[0] >= left && bp[0] <= right && bp[1] >= top && bp[1] <= bot; 
+};
+
+QNode.prototype.contentIn = function(left, top, right, bot){
+	var bp = this.contentLoc;
+	return bp[0] >= left && bp[0] <= right && bp[1] >= top && bp[1] <= bot; 
+};
+
+QNode.prototype.deployContent = function(){
+	var output = [];
+	var process = this.content;
+	var next = null;
+	while(next = process.pop()){
+		if(next.status == 'root'){
+			next.content.map(function(c){
+				process.push(c);
+			});
+		}
+		else {
+			if(next.content) output.push(next.content);
+		}
+	}
+	return output;
+}
--- test.js
+++ test.js
@@ -17,7 +17,7 @@ it('Should have a single leaf with breakPoint of [0.5,0.5]', 'equal', 0.5, funct
 	return tree.root.breakPoint[0];
 });
 
-it('Should get SW', 'equal', 'SW', function(){
+it('Should get SW', 'equal', 2, function(){
 	var leaf = new QNode(0, [0.5,0.5]);
 	return leaf.getQuad([0.1, 0.75]);
 });
@@ -32,13 +32,39 @@ it('Should had an object in the NW quad of the tree', 'equal', 'truc', function(
 	var tree = new QTree();
 	tree.add('truc', [0.3,0.12]);
 	tree.add('michel', [0.6, 0.12]);
-	return tree.root.NW.content;
+	return tree.root.content[0].content;
 });
 
 it('Should had an object in the NE quad of the tree', 'equal', 'michel', function(){
 	var tree = new QTree();
 	tree.add('truc', [0.3,0.12]);
 	tree.add('michel', [0.6, 0.12]);
-	console.log(tree);
-	return tree.root.NE.content;
+	return tree.root.content[1].content;
+});
+
+it('Should return true', 'equal', true, function(){
+	var leaf = new QNode(0, [0.2,0.6]);
+	return leaf.bpIn(0.1,0.5,0.5,0.7);
+});
+
+it('Should return false', 'equal', false, function(){
+	var leaf = new QNode(0, [0.31,0.6]);
+	return leaf.bpIn(0.5, 0.1, 0.7, 0.3);
+});
+
+it('Should deploy two objects', 'equal', 2, function(){
+	var tree = new QTree();
+	tree.add('truc', [0.3,0.12]);
+	tree.add('michel', [0.6, 0.12]);
+	return tree.root.deployContent().length;
+});
+
+it('Should deploy three objects', 'equal', 3, function(){
+	var tree = new QTree();
+	tree.add('truc', [0.3, 0.12]);
+	tree.add('michel', [0.6, 0.12]);
+	tree.add('kawai', [0.59, 0.13]);
+	tree.add('jean', [0.45, 0.5]);
+	tree.add('chose', [0.35, 0.25]);
+	return tree.findIn(0.4, 0.1, 0.65, 0.6).length;
 });
