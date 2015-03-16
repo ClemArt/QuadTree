315e414bb42a75a473b35f69b9c0482cf7c0ec9e
Clement <clem83f@hotmail.fr>
2015-03-16 21:25:08 +0100

    QTree working, with support for finding all objects in an area

--- qTree.js
+++ qTree.js
@@ -1,116 +1,188 @@
 /*
 Small library for the manipulation of Quadratic Trees, very useful for speed up and optimisation of finding algorithms
-Used with a discriminant normal location between [0,1] to locate the different sub-trees
 Copyright 2015, Clement Artaud 
-Version : 1.0.0
+Version : 2.0.0
 */
 
-function QTree(){
-	this.root = new QNode(0, [0.5, 0.5]);
-}
-
-QTree.prototype.add = function(content, contentLoc){
-	this.root.put(content, contentLoc);
+/* QTree object, created with a bounding box object, a depth, an optional max depth and max objects
+@param Object bbox : {x, y, width, height} bounding box of the tree
+@param (optional) Number depth, maxDepth, maxObjects
+@param (optional) Object QTree parent : reference to the parent node, default null
+*/
+function QTree(bbox, depth, maxDepth, maxObjects, parent){
+	this.x = bbox.x;
+	this.y = bbox.y;
+	this.width = bbox.width;
+	this.height = bbox.height;
+	this.depth = (typeof depth !== 'number') ? 0 : depth;
+	
+	this.maxDepth = (typeof maxDepth !== 'number') ? 10 : maxDepth;
+	this.maxObjects = (typeof maxObjects !== 'number') ? 10 : maxObjects;
+	
+	this.nodes = [];
+	this.objects = [];
+	this.parent = (typeof parent === 'undefined') ? undefined : parent;
 };
 
-QTree.prototype.findIn = function(left, top, right, bot){
-	var output = [];
-	var process = [this.root];
-	var next = null;
-	while(next = process.pop()){
-		if(next.status == 'root'){ //Explore the tree to register new elements
-			if(next.bpIn(left, top, right, bot)){
-				next.content.map(function(c){
-					process.push(c);
-				});
-			}
-			else {
-				var index = [next.getQuad([left, top]), next.getQuad([right, bot])];
-				for(iii=index[0]; iii<= index[1]; iii++){
-					process.push(next.content[iii]);
-				}
+/* Finds all objects which can collide a given bounding box
+@return [Object] : array of objects in the tree around the bounding box
+*/
+QTree.prototype.find = function(bbox){
+	var output = this.objects;
+	
+	/*
+	for(iii=0; iii<this.objects.length; iii++){
+		var obj = this.objects[iii];
+		if(this.cross(bbox, obj.bbox)){
+			output.push(obj.object);
+		}
+	}
+	*/
+	
+	//If there are subtrees
+	if(this.nodes.length === 4){
+		//Test node index for bbox, and add the node, or all of them if no fitting
+		var index = this.getIndex(bbox);
+		if(index === -1){
+			for(iii=0; iii<4; iii++){
+				output.concat(this.nodes[iii].find(bbox));
 			}
 		}
 		else {
-			if(next.content && next.contentIn(left, top, right, bot)) output.push(next.content);
+			output.concat(this.nodes[index].find(bbox));
 		}
 	}
 	
 	return output;
 };
 
-QTree.prototype.deploy = function(){
-	return this.root.deployContent();
-};
-
-function QNode(level, breakPoint){
-	this.breakPoint = breakPoint;
-	this.status = 'leaf';
-	this.content = null;
-	this.contentLoc = null;
-	this.lvl = level;
-}
-
-QNode.prototype.put = function(content, contentLoc){
-	if(this.status == 'leaf' && ! this.content){
-		this.content = content;
-		this.contentLoc = contentLoc;
-	}
-	else if(this.status == 'leaf'){
-		this.expand();
-		this.content[this.getQuad(contentLoc)].put(content, contentLoc);
-	}
-	else {
-		this.content[this.getQuad(contentLoc)].put(content, contentLoc);
-	}
-};
-
-QNode.prototype.expand = function(){
-	var level = this.lvl + 1;
-	var bp = this.breakPoint;
-	
-	this.status = 'root';
-	this.content = [this.content];
-	this.content.push(new QNode(level, [bp[0] - 0.25/level, bp[1] - 0.25/level]));
-	this.content.push(new QNode(level, [bp[0] + 0.25/level, bp[1] - 0.25/level]));
-	this.content.push(new QNode(level, [bp[0] - 0.25/level, bp[1] + 0.25/level]));
-	this.content.push(new QNode(level, [bp[0] + 0.25/level, bp[1] + 0.25/level]));
-	
-	var cont = this.content.shift();
-	this.content[this.getQuad(this.contentLoc)].put(cont, this.contentLoc);
-	this.contentLoc = null;
-};
-
-QNode.prototype.getQuad = function(contentLoc){
-	var bp = this.breakPoint;
-	var loc = (contentLoc[1] <= bp[1]) ? 0 : 2;
-	loc += (contentLoc[0] <= bp[0]) ? 0 : 1;
-	return loc;
-};
-
-QNode.prototype.bpIn = function(left, top, right, bot){
-	var bp = this.breakPoint;
-	return bp[0] >= left && bp[0] <= right && bp[1] >= top && bp[1] <= bot; 
-};
-
-QNode.prototype.contentIn = function(left, top, right, bot){
-	var bp = this.contentLoc;
-	return bp[0] >= left && bp[0] <= right && bp[1] >= top && bp[1] <= bot; 
-};
-
-QNode.prototype.deployContent = function(){
-	var output = [];
-	var process = this.content;
-	var next = null;
-	while(next = process.pop()){
-		if(next.status == 'root'){
-			next.content.map(function(c){
-				process.push(c);
-			});
+/* Is the object crossing the bounding box
+@return Boolean : true if the object is crossing
+*/
+QTree.prototype.cross = function(bbox, objbox){
+	//Horizontal Crossing
+	if( ! (objbox.x > bbox.x+bbox.width || objbox.x+objbox.width < bbox.x)){
+		//Vertical Crossing
+		if( ! (objbox.y > bbox.y+bbox.height || objbox.y+objbox.height < bbox.y)){
+			//Effectively crossing !
+			return true;
 		}
+	}
+	return false;
+};
+
+/* Adds an object with his bounding box in the tree
+@return Boolean : true if object is added
+*/
+QTree.prototype.add = function(object, bbox){
+	//Add the object to the current tree, if there is no subtree
+	if(this.nodes.length === 0){
+		//If we don't overwhelm the current node, add to it
+		if(this.objects.length < this.maxObjects){
+			this.objects.push({object: object, bbox: bbox});
+			return true;
+		}
+		//Else split the node if not max depth
+		if(this.depth < this.maxDepth){
+			this.divide();
+		}
+		//If not possible throw an error for max depth reached
 		else {
-			if(next.content) output.push(next.content);
+			throw new Error('Max depth reached, tree is full !');
 		}
 	}
-	return output;
-}
+	//Push the content to the next tree, or put it there if there is no fitting inside a subtree
+	var index = this.getIndex(bbox);
+	if(index === -1){ //No fit in a subtree
+		this.objects.push({object: object, bbox: bbox});
+		return true;
+	}
+	//Lastly push to the adequate subtree
+	return this.nodes[index].add(object, bbox);
+};
+
+/* Divides a node in 4 sub-nodes and pushes down the content of the node */
+QTree.prototype.divide = function(){
+	var subWidth = this.width/2;
+	var subHeight = this.height/2;
+	
+	//TopLeft
+	this.nodes[0] = new QTree({
+		x: this.x,
+		y: this.y,
+		width: subWidth,
+		height: subHeight,
+	}, this.depth+1, this.maxDepth, this.maxObjects, this);
+	
+	//TopRight
+	this.nodes[1] = new QTree({
+		x: this.x+subWidth,
+		y: this.y,
+		width: subWidth,
+		height: subHeight,
+	}, this.depth+1, this.maxDepth, this.maxObjects, this);
+	
+	//BotLeft
+	this.nodes[2] = new QTree({
+		x: this.x,
+		y: this.y+subHeight,
+		width: subWidth,
+		height: subHeight,
+	}, this.depth+1, this.maxDepth, this.maxObjects, this);
+	
+	//BotRight
+	this.nodes[3] = new QTree({
+		x: this.x+subWidth,
+		y: this.y+subHeight,
+		width: subWidth,
+		height: subHeight,
+	}, this.depth+1, this.maxDepth, this.maxObjects, this);
+	
+	//Throw down the content of the tree, except if there is no fitting
+	var stayHere = [], next = null;
+	while(next = this.objects.pop()){
+		//Push the content to the next tree, or put it there if there is no fitting inside a subtree
+		var index = this.getIndex(next.bbox);
+		if(index === -1){ //No fit in a subtree
+			stayHere.push(next);
+		}
+		//Lastly push to the adequate subtree
+		else this.nodes[index].add(next.object, next.bbox);
+	}
+	this.objects = stayHere;
+};
+
+/* Gets the subtree index of a given bounding box
+@return Number index : index of the node, -1 if the content does not fit in a subtree
+*/
+QTree.prototype.getIndex = function(bbox){
+	var verticalMid = this.x+this.width/2;
+	var horizontalMid = this.y+this.height/2;
+	
+	var index = -1;
+	
+	//Top section
+	if(bbox.y+bbox.height < horizontalMid){
+		//Top Left
+		if(bbox.x+bbox.width < verticalMid){
+			index = 0;
+		}
+		//Top Right
+		else if(bbox.x >= verticalMid){
+			index = 1;
+		}
+	}
+	//Bot section
+	else if(bbox.y >= horizontalMid){
+		//Bot Left
+		if(bbox.x+bbox.width < verticalMid){
+			index = 2;
+		}
+		//Bot Right
+		else if(bbox.x >= verticalMid){
+			index = 3;
+		}
+	}
+	
+	return index;
+};
--- test.js
+++ test.js
@@ -1,70 +1,111 @@
+var bbox = {
+	x: 0,
+	y: 0,
+	width: 500,
+	height: 500,
+};
+
 it('Should create a tree', null, null, function(){
-	var tree = new QTree();
+	var tree = new QTree(bbox);
 });
 
-it('Should have a single leaf', 'equal', 'leaf', function(){
-	var tree = new QTree();
-	return tree.root.status;
+it('Should return the depth (0) of the tree', 'equal', 0, function(){
+	var tree = new QTree(bbox);
+	return tree.depth;
 });
 
-it('Should have a single leaf with lvl 0', 'equal', '0', function(){
-	var tree = new QTree();
-	return tree.root.lvl;
+it('Should add an object to a tree', 'equal', true, function(){
+	var tree = new QTree(bbox);
+	var objbox = {
+		x: 12,
+		y: 456,
+		width: 12,
+		height: 45,
+	};
+	
+	return tree.add('trucMuche', objbox);
 });
 
-it('Should have a single leaf with breakPoint of [0.5,0.5]', 'equal', 0.5, function(){
-	var tree = new QTree();
-	return tree.root.breakPoint[0];
+it('Should add an object to a subtree', 'equal', true, function(){
+	var tree = new QTree(bbox, null, null, 1);
+	var objbox1 = {
+		x: Math.random()*500,
+		y: Math.random()*500,
+		width: Math.random()*10,
+		height: Math.random()*10,
+	};
+	var objbox2 = {
+		x: Math.random()*500,
+		y: Math.random()*500,
+		width: Math.random()*10,
+		height: Math.random()*10,
+	};
+	
+	tree.add('trucMuche', objbox1);
+	return tree.add('frakgrak', objbox2);
 });
 
-it('Should get SW', 'equal', 2, function(){
-	var leaf = new QNode(0, [0.5,0.5]);
-	return leaf.getQuad([0.1, 0.75]);
+it('Should throw an error because too many depths', null, null, function(){
+	var tree = new QTree(bbox, null, 5, 2);
+	
+	for(iii=0; iii<2000; iii++){
+		var objbox = {
+			x: Math.random()*500,
+			y: Math.random()*500,
+			width: Math.random()*10,
+			height: Math.random()*10,
+		};
+		tree.add('trucMuche'+iii, objbox);
+	}
+	console.log(tree);
 });
 
-it('Should had an object in the root of the tree', 'equal', 'truc', function(){
-	var tree = new QTree();
-	tree.add('truc', [0.3,0.12]);
-	return tree.root.content;
+it('Should add many objects to subtrees', null, null, function(){
+	var tree = new QTree(bbox, null, null, 1);
+	
+	for(iii=0; iii<200; iii++){
+		var objbox = {
+			x: Math.random()*500,
+			y: Math.random()*500,
+			width: Math.random()*40,
+			height: Math.random()*40,
+		};
+		tree.add('trucMuche'+iii, objbox);
+	}
 });
 
-it('Should had an object in the NW quad of the tree', 'equal', 'truc', function(){
-	var tree = new QTree();
-	tree.add('truc', [0.3,0.12]);
-	tree.add('michel', [0.6, 0.12]);
-	return tree.root.content[0].content;
+it('Should add many objects to subtrees and render it', null, null, function(){
+	var tree = new QTree(bbox, null, 10, 1);
+	
+	for(iii=0; iii<2000; iii++){
+		var objbox = {
+			x: Math.random()*500,
+			y: Math.random()*500,
+			width: Math.random()*10,
+			height: Math.random()*10,
+		};
+		tree.add('trucMuche'+iii, objbox);
+	}
+	console.log(tree);
 });
 
-it('Should had an object in the NE quad of the tree', 'equal', 'michel', function(){
-	var tree = new QTree();
-	tree.add('truc', [0.3,0.12]);
-	tree.add('michel', [0.6, 0.12]);
-	return tree.root.content[1].content;
-});
-
-it('Should return true', 'equal', true, function(){
-	var leaf = new QNode(0, [0.2,0.6]);
-	return leaf.bpIn(0.1,0.5,0.5,0.7);
-});
-
-it('Should return false', 'equal', false, function(){
-	var leaf = new QNode(0, [0.31,0.6]);
-	return leaf.bpIn(0.5, 0.1, 0.7, 0.3);
-});
-
-it('Should deploy two objects', 'equal', 2, function(){
-	var tree = new QTree();
-	tree.add('truc', [0.3,0.12]);
-	tree.add('michel', [0.6, 0.12]);
-	return tree.root.deployContent().length;
-});
-
-it('Should deploy three objects', 'equal', 3, function(){
-	var tree = new QTree();
-	tree.add('truc', [0.3, 0.12]);
-	tree.add('michel', [0.6, 0.12]);
-	tree.add('kawai', [0.59, 0.13]);
-	tree.add('jean', [0.45, 0.5]);
-	tree.add('chose', [0.35, 0.25]);
-	return tree.findIn(0.4, 0.1, 0.65, 0.6).length;
+it('Should add many objects to tree and return an array of objects in a box', null, null, function(){
+	var tree = new QTree(bbox, null, null, 3);
+	
+	for(iii=0; iii<2000; iii++){
+		var objbox = {
+			x: Math.random()*500,
+			y: Math.random()*500,
+			width: Math.random()*1,
+			height: Math.random()*1,
+		};
+		tree.add('trucMuche'+iii, objbox);
+	}
+	
+	console.log(tree.find({
+		x:50,
+		y:150,
+		width: 168,
+		height: 148,
+	}));
 });
