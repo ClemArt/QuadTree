91b97d9b0e2656e28a9c48af71a54024c23e928f
Clement <clem83f@hotmail.fr>
2015-03-18 00:28:33 +0100

    Removed support for sized objects, only points now. Also a better algorithm to search and find which quadrant intersect the searching box. Now the quad tree takes his bounds instead of a width and height

--- qTree.js
+++ qTree.js
@@ -4,185 +4,126 @@ Copyright 2015, Clement Artaud
 Version : 2.0.0
 */
 
-/* QTree object, created with a bounding box object, a depth, an optional max depth and max objects
-@param Object bbox : {x, y, width, height} bounding box of the tree
-@param (optional) Number depth, maxDepth, maxObjects
+/* QTree object, created with a bounding box object, a depth, an optional max depth and parent
+@param Number x1, y1, x2, y2 : bounds of the tree
+@param (optional) Number depth, maxDepth
 @param (optional) Object QTree parent : reference to the parent node, default null
 */
-function QTree(bbox, depth, maxDepth, maxObjects, parent){
-	this.x = bbox.x;
-	this.y = bbox.y;
-	this.width = bbox.width;
-	this.height = bbox.height;
+function QTree(x1, y1, x2, y2, depth, maxDepth, parent){
+	this.x1 = x1;
+	this.y1 = y1;
+	this.x2 = x2;
+	this.y2 = y2;
+	
 	this.depth = (typeof depth !== 'number') ? 0 : depth;
 	
-	this.maxDepth = (typeof maxDepth !== 'number') ? 10 : maxDepth;
-	this.maxObjects = (typeof maxObjects !== 'number') ? 10 : maxObjects;
+	this.maxDepth = (typeof maxDepth !== 'number') ? 50 : maxDepth;
 	
 	this.nodes = [];
-	this.objects = [];
+	
+	this.object;
+	this.objX;
+	this.objY;
+	
 	this.parent = (typeof parent === 'undefined') ? undefined : parent;
 };
 
 /* Finds all objects which can collide a given bounding box
 @return [Object] : array of objects in the tree around the bounding box
 */
-QTree.prototype.find = function(bbox){
-	var output = this.objects;
+QTree.prototype.find = function(x1, y1, x2, y2){
+	var output = [];
 	
-	/*
-	for(iii=0; iii<this.objects.length; iii++){
-		var obj = this.objects[iii];
-		if(this.cross(bbox, obj.bbox)){
-			output.push(obj.object);
-		}
+	if(this.objX >= x1 && this.objX <= x2 && this.objY >= y1 && this.objY <= y2){
+		output.push(this.object);
 	}
-	*/
 	
 	//If there are subtrees
-	if(this.nodes.length === 4){
+	if(this.nodes.length > 0){
 		//Test node index for bbox, and add the node, or all of them if no fitting
-		var index = this.getIndex(bbox);
-		if(index === -1){
-			for(iii=0; iii<4; iii++){
-				output.concat(this.nodes[iii].find(bbox));
-			}
-		}
-		else {
-			output.concat(this.nodes[index].find(bbox));
-		}
+		var index = this.getIndex(x1, y1, x2, y2);
+		index.forEach(function(iii){
+			output.concat(this.nodes[iii].find(x1, y1, x2, y2));
+		});
 	}
 	
 	return output;
 };
 
-/* Is the object crossing the bounding box
-@return Boolean : true if the object is crossing
-*/
-QTree.prototype.cross = function(bbox, objbox){
-	//Horizontal Crossing
-	if( ! (objbox.x > bbox.x+bbox.width || objbox.x+objbox.width < bbox.x)){
-		//Vertical Crossing
-		if( ! (objbox.y > bbox.y+bbox.height || objbox.y+objbox.height < bbox.y)){
-			//Effectively crossing !
-			return true;
-		}
-	}
-	return false;
-};
-
-/* Adds an object with his bounding box in the tree
+/* Adds an object with his position in the tree
 @return Boolean : true if object is added
 */
-QTree.prototype.add = function(object, bbox){
-	//Add the object to the current tree, if there is no subtree
-	if(this.nodes.length === 0){
-		//If we don't overwhelm the current node, add to it
-		if(this.objects.length < this.maxObjects){
-			this.objects.push({object: object, bbox: bbox});
-			return true;
-		}
-		//Else split the node if not max depth
-		if(this.depth < this.maxDepth){
-			this.divide();
-		}
-		//If not possible throw an error for max depth reached
-		else {
-			throw new Error('Max depth reached, tree is full !');
-		}
+QTree.prototype.add = function(object, x, y){
+	//Add the object to the next tree if it exists
+	if(this.nodes.length > 0){
+		var index = this.getIndex(x, y, x, y);
+		this.nodes[index].add(object, x, y);
 	}
-	//Push the content to the next tree, or put it there if there is no fitting inside a subtree
-	var index = this.getIndex(bbox);
-	if(index === -1){ //No fit in a subtree
-		this.objects.push({object: object, bbox: bbox});
-		return true;
-	}
-	//Lastly push to the adequate subtree
-	return this.nodes[index].add(object, bbox);
 };
 
-/* Divides a node in 4 sub-nodes and pushes down the content of the node */
+/* Divides a node in 4 sub-nodes */
 QTree.prototype.divide = function(){
-	var subWidth = this.width/2;
-	var subHeight = this.height/2;
+	var subx = (this.x1 + this.x2)/2;
+	var suby = (this.y1 + this.y2)/2;
 	
 	//TopLeft
-	this.nodes[0] = new QTree({
-		x: this.x,
-		y: this.y,
-		width: subWidth,
-		height: subHeight,
-	}, this.depth+1, this.maxDepth, this.maxObjects, this);
+	this.nodes[0] = new QTree(this.x1, this.y1, subx, suby, this.depth+1, this.maxDepth, this);
 	
 	//TopRight
-	this.nodes[1] = new QTree({
-		x: this.x+subWidth,
-		y: this.y,
-		width: subWidth,
-		height: subHeight,
-	}, this.depth+1, this.maxDepth, this.maxObjects, this);
+	this.nodes[1] = new QTree(subx, this.y1, this.x2, suby, this.depth+1, this.maxDepth, this);
 	
 	//BotLeft
-	this.nodes[2] = new QTree({
-		x: this.x,
-		y: this.y+subHeight,
-		width: subWidth,
-		height: subHeight,
-	}, this.depth+1, this.maxDepth, this.maxObjects, this);
+	this.nodes[2] = new QTree(this.x1, suby, subx, this.y2, this.depth+1, this.maxDepth, this);
 	
 	//BotRight
-	this.nodes[3] = new QTree({
-		x: this.x+subWidth,
-		y: this.y+subHeight,
-		width: subWidth,
-		height: subHeight,
-	}, this.depth+1, this.maxDepth, this.maxObjects, this);
-	
-	//Throw down the content of the tree, except if there is no fitting
-	var stayHere = [], next = null;
-	while(next = this.objects.pop()){
-		//Push the content to the next tree, or put it there if there is no fitting inside a subtree
-		var index = this.getIndex(next.bbox);
-		if(index === -1){ //No fit in a subtree
-			stayHere.push(next);
-		}
-		//Lastly push to the adequate subtree
-		else this.nodes[index].add(next.object, next.bbox);
-	}
-	this.objects = stayHere;
+	this.nodes[3] = new QTree(subx, suby, this.x2, this.y2, this.depth+1, this.maxDepth, this);
 };
 
 /* Gets the subtree index of a given bounding box
-@return Number index : index of the node, -1 if the content does not fit in a subtree
+@return [Number] index : index of the nodes which cover the box
 */
-QTree.prototype.getIndex = function(bbox){
-	var verticalMid = this.x+this.width/2;
-	var horizontalMid = this.y+this.height/2;
+QTree.prototype.getIndex = function(x1, y1, x2, y2){
+	var verticalMid = (this.x1+this.x2)/2;
+	var horizontalMid = (this.y1+this.y2)/2;
 	
-	var index = -1;
+	//Discriminate section fitting
+	var top = (y2 < horizontalMid);
+	var bot = (y1 >= horizontalMid);
+	var left = (x2 < verticalMid);
+	var right = (x1 >= verticalMid);
 	
-	//Top section
-	if(bbox.y+bbox.height < horizontalMid){
-		//Top Left
-		if(bbox.x+bbox.width < verticalMid){
-			index = 0;
-		}
-		//Top Right
-		else if(bbox.x >= verticalMid){
-			index = 1;
-		}
+	//Top Left
+	if(top && left){
+		return [0];
 	}
-	//Bot section
-	else if(bbox.y >= horizontalMid){
-		//Bot Left
-		if(bbox.x+bbox.width < verticalMid){
-			index = 2;
-		}
-		//Bot Right
-		else if(bbox.x >= verticalMid){
-			index = 3;
-		}
+	//Top Right
+	else if(top && right){
+		return [1];
+	}
+	//Bot Left
+	else if(bot && left){
+		return [2];
+	}
+	//Bot Right
+	else if(bot && right){
+		return [3];
+	}
+	//Just Top
+	else if(top){
+		return [0,1];
+	}
+	//Just Bot
+	else if(bot){
+		return [2,3];
+	}
+	//Just Left
+	else if(left){
+		return [0,2];
+	}
+	//Just Right
+	else if(right){
+		return [1,3];
 	}
 	
-	return index;
+	return [0,1,2,3];
 };
