403ab7c4d2e45079442af9423277f4aa828a497a
Clement <clem83f@hotmail.fr>
2015-03-22 00:07:16 +0100

    Add function OK, visit function first tests. It needs a callback on the add function to act on passing nodes. Also needs a possibility to store an object in a root node, not only leafs

--- qTree.js
+++ qTree.js
@@ -9,7 +9,7 @@ Version : 2.0.0
 @param (optional) Number depth, maxDepth
 @param (optional) Object QTree parent : reference to the parent node, default null
 */
-function QTree(x1, y1, x2, y2, depth, maxDepth, parent){
+function QTree(x1, y1, x2, y2, maxDepth, parent, depth){
 	this.x1 = x1;
 	this.y1 = y1;
 	this.x2 = x2;
@@ -21,11 +21,26 @@ function QTree(x1, y1, x2, y2, depth, maxDepth, parent){
 	
 	this.nodes = [];
 	
-	this.object;
-	this.objX;
-	this.objY;
+	this.object = null;
+	this.objX = null;
+	this.objY = null;
 	
 	this.parent = (typeof parent === 'undefined') ? undefined : parent;
+};
+
+/* Visits the nodes recursively and apply a callback function on the node.
+@param Function : callback f(x1,y1,x2,y2,object, x, y)
+Visit the children if the callback returns true
+*/
+QTree.prototype.visit = function(callback){
+	var next = callback.call(this, this.x1, this.y1, this.x2, this.y2, this.object, this.objX, this.objY);
+	if(next){
+		if(this.nodes.length > 0){
+			this.nodes.forEach(function(node){
+				node.visit(callback);
+			});
+		}
+	}
 };
 
 /* Finds all objects which can collide a given bounding box
@@ -34,6 +49,7 @@ function QTree(x1, y1, x2, y2, depth, maxDepth, parent){
 QTree.prototype.find = function(x1, y1, x2, y2){
 	var output = [];
 	
+	//If the object is in the box
 	if(this.objX >= x1 && this.objX <= x2 && this.objY >= y1 && this.objY <= y2){
 		output.push(this.object);
 	}
@@ -42,8 +58,9 @@ QTree.prototype.find = function(x1, y1, x2, y2){
 	if(this.nodes.length > 0){
 		//Test node index for bbox, and add the node, or all of them if no fitting
 		var index = this.getIndex(x1, y1, x2, y2);
+		var self = this;
 		index.forEach(function(iii){
-			output.concat(this.nodes[iii].find(x1, y1, x2, y2));
+			output = output.concat(self.nodes[iii].find(x1, y1, x2, y2));
 		});
 	}
 	
@@ -56,9 +73,29 @@ QTree.prototype.find = function(x1, y1, x2, y2){
 QTree.prototype.add = function(object, x, y){
 	//Add the object to the next tree if it exists
 	if(this.nodes.length > 0){
-		var index = this.getIndex(x, y, x, y);
-		this.nodes[index].add(object, x, y);
+		var index = this.getIndex(x, y, x, y)[0]; // The index can only be one precise subtree (point object)
+		return this.nodes[index].add(object, x, y);
 	}
+	//If the object is not empty
+	if(this.object){
+		this.divide();
+		//Push down the object
+		var index = this.getIndex(this.objX, this.objY, this.objX, this.objY);
+		this.nodes[index].add(this.object, this.objX, this.objY);
+		
+		this.object = null;
+		this.objX = null;
+		this.objY = null;
+		
+		var index = this.getIndex(x, y, x, y)[0];
+		return this.nodes[index].add(object, x, y);
+	}
+	//Add the object to the curent node
+	this.object = object;
+	this.objX = x;
+	this.objY = y;
+	
+	return true;
 };
 
 /* Divides a node in 4 sub-nodes */
@@ -67,16 +104,16 @@ QTree.prototype.divide = function(){
 	var suby = (this.y1 + this.y2)/2;
 	
 	//TopLeft
-	this.nodes[0] = new QTree(this.x1, this.y1, subx, suby, this.depth+1, this.maxDepth, this);
+	this.nodes[0] = new QTree(this.x1, this.y1, subx, suby, this.maxDepth, this, this.depth+1);
 	
 	//TopRight
-	this.nodes[1] = new QTree(subx, this.y1, this.x2, suby, this.depth+1, this.maxDepth, this);
+	this.nodes[1] = new QTree(subx, this.y1, this.x2, suby, this.maxDepth, this, this.depth+1);
 	
 	//BotLeft
-	this.nodes[2] = new QTree(this.x1, suby, subx, this.y2, this.depth+1, this.maxDepth, this);
+	this.nodes[2] = new QTree(this.x1, suby, subx, this.y2, this.maxDepth, this, this.depth+1);
 	
 	//BotRight
-	this.nodes[3] = new QTree(subx, suby, this.x2, this.y2, this.depth+1, this.maxDepth, this);
+	this.nodes[3] = new QTree(subx, suby, this.x2, this.y2, this.maxDepth, this, this.depth+1);
 };
 
 /* Gets the subtree index of a given bounding box
--- test.js
+++ test.js
@@ -1,111 +1,74 @@
-var bbox = {
-	x: 0,
-	y: 0,
-	width: 500,
-	height: 500,
-};
-
 it('Should create a tree', null, null, function(){
-	var tree = new QTree(bbox);
+	var tree = new QTree(0,0,500,500);
 });
 
 it('Should return the depth (0) of the tree', 'equal', 0, function(){
-	var tree = new QTree(bbox);
+	var tree = new QTree(0,0,500,500);
 	return tree.depth;
 });
 
 it('Should add an object to a tree', 'equal', true, function(){
-	var tree = new QTree(bbox);
-	var objbox = {
-		x: 12,
-		y: 456,
-		width: 12,
-		height: 45,
-	};
-	
-	return tree.add('trucMuche', objbox);
+	var tree = new QTree(0,0,500,500);	
+	return tree.add('trucMuche', 12, 456);
 });
 
 it('Should add an object to a subtree', 'equal', true, function(){
-	var tree = new QTree(bbox, null, null, 1);
-	var objbox1 = {
-		x: Math.random()*500,
-		y: Math.random()*500,
-		width: Math.random()*10,
-		height: Math.random()*10,
-	};
-	var objbox2 = {
-		x: Math.random()*500,
-		y: Math.random()*500,
-		width: Math.random()*10,
-		height: Math.random()*10,
-	};
+	var tree = new QTree(0,0,500,500);
 	
-	tree.add('trucMuche', objbox1);
-	return tree.add('frakgrak', objbox2);
+	tree.add('trucMuche', 12,456);
+	return tree.add('frakgrak', 78,65);
 });
 
 it('Should throw an error because too many depths', null, null, function(){
-	var tree = new QTree(bbox, null, 5, 2);
+	var tree = new QTree(0,0,500,500, 2);
 	
 	for(iii=0; iii<2000; iii++){
-		var objbox = {
-			x: Math.random()*500,
-			y: Math.random()*500,
-			width: Math.random()*10,
-			height: Math.random()*10,
-		};
-		tree.add('trucMuche'+iii, objbox);
+		var x = Math.random()*500,
+			 y = Math.random()*500;
+			 
+		tree.add('trucMuche'+iii, x, y);
 	}
 	console.log(tree);
 });
 
 it('Should add many objects to subtrees', null, null, function(){
-	var tree = new QTree(bbox, null, null, 1);
-	
-	for(iii=0; iii<200; iii++){
-		var objbox = {
-			x: Math.random()*500,
-			y: Math.random()*500,
-			width: Math.random()*40,
-			height: Math.random()*40,
-		};
-		tree.add('trucMuche'+iii, objbox);
-	}
-});
-
-it('Should add many objects to subtrees and render it', null, null, function(){
-	var tree = new QTree(bbox, null, 10, 1);
+	var tree = new QTree(0,0,500,500);
 	
 	for(iii=0; iii<2000; iii++){
-		var objbox = {
-			x: Math.random()*500,
-			y: Math.random()*500,
-			width: Math.random()*10,
-			height: Math.random()*10,
-		};
-		tree.add('trucMuche'+iii, objbox);
+		var x = Math.random()*500,
+			 y = Math.random()*500;
+			 
+		tree.add('trucMuche'+iii, x, y);
 	}
 	console.log(tree);
 });
 
 it('Should add many objects to tree and return an array of objects in a box', null, null, function(){
-	var tree = new QTree(bbox, null, null, 3);
+	var tree = new QTree(0,0,500,500);
 	
-	for(iii=0; iii<2000; iii++){
-		var objbox = {
-			x: Math.random()*500,
-			y: Math.random()*500,
-			width: Math.random()*1,
-			height: Math.random()*1,
-		};
-		tree.add('trucMuche'+iii, objbox);
+	for(iii=0; iii<200; iii++){
+		var x = Math.random()*500,
+			 y = Math.random()*500;
+			 
+		tree.add('trucMuche'+iii, x, y);
 	}
 	
-	console.log(tree.find({
-		x:50,
-		y:150,
-		width: 168,
-		height: 148,
-	}));
+	console.log(tree);
+	console.log(tree.find(50,50,450,450));
+});
+
+it('Should add many objects to tree and visit it', null, null, function(){
+	var tree = new QTree(0,0,500,500);
+	
+	for(iii=0; iii<50; iii++){
+		var x = Math.random()*500,
+			 y = Math.random()*500;
+			 
+		tree.add('trucMuche'+iii, x, y);
+	}
+	
+	tree.visit(function(x1, y1, x2, y2, obj, x, y){
+		if(obj) console.log(obj+'  '+x+'  '+y);
+		return (x1 > y1 && x2>y2);
+	});
 });
